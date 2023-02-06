ff56c2291944f8a6e3adfbd95a3c9c65039020d3
Mike Hartington <mikehartington@gmail.com>
2023-02-06 10:59:06 -0500

    updates

---
+++ config/nvim/lua/mh/completion/init.lua
@@ -0,0 +1,76 @@
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  mapping = {
+    ["<CR>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+          local entry = cmp.get_active_entry()
+         if not entry then
+            fallback()
+          else
+            cmp.confirm({  
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+          })
+          end
+      else
+        fallback()
+      end
+        end),
+    ["<C-n>"] = cmp.mapping(
+      {
+        c = function()
+          if cmp.visible() then
+          cmp.select_next_item()
+          else
+            vim.api.nvim_feedkeys(t("<Down>"), "n", true)
+          end
+        end,
+        i = function(fallback)
+          if cmp.visible() then
+          cmp.select_next_item()
+          else
+            fallback()
+          end
+        end
+      }
+    ),
+    ["<C-p>"] = cmp.mapping(
+      {
+        c = function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            vim.api.nvim_feedkeys(t("<Up>"), "n", true)
+          end
+        end,
+        i = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end
+      }
+    )
+  },
+  sources = {
+    {name = "nvim_lsp"},
+    {name = "luasnip"},
+    {name = "spell"},
+    {name = "nvim_lsp_signature_help"}
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect',
+  }
+}
